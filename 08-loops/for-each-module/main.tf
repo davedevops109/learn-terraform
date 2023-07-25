a1cc3902afde74fd2a959114b2f3aeddd54dfefe
EPAPRA <dave.devops109@gmail.com>
2023-07-25 14:39:42 +0530

    multibranch commit

---
+++ 08-loops/for-each-module/main.tf
@@ -0,0 +1,23 @@
+variable "components" {
+  default = {
+    cart = {
+      name = "cart",
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name = "catalogue",
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+module "ec2" {
+  source = "./module"
+
+  for_each = var.components
+  instance_type = each.value.instance_type
+  name = each.value.name
+}
+
+## always iterate modules not resources
+## always map the data and use for_each loop..
