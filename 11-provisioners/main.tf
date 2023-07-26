4cd120d0213e94099a28330dc3f1a7486d2fd765
EPAPRA <dave.devops109@gmail.com>
2023-07-26 13:07:35 +0530

    multibranch commit

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -8,16 +8,19 @@ data "aws_ami" "centos8" {
 
 }
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.centos8.id
-  instance_type = "t3.micro"
+  ami                    = data.aws_ami.centos8.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "test-centos8"
   }
+}
 
+resource "null_resource" "provision" {
   provisioner "remote-exec" {
     connection {
-      host = self.public_ip
+      host = aws_instance.web.public_ip
       user = "centos"
       password = "DevOps321"
     }
